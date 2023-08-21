8155738797015d237d1e017477dc39a16ede8134
bernardkissi <bernardkissi@live.com>
2023-08-21 02:47:45 +0200

    wip: dashboard updated

--- resources/js/Pages/Dashboard/UserDashboard.vue
+++ resources/js/Pages/Dashboard/UserDashboard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex w-full min-h-screen bg-gray-100">
-    <aside class="relative w-64 bg-gray-100 px-6 py-4">
+  <div class="flex w-full min-h-screen bg-gray-100 px-2 py-6">
+    <aside class="relative w-64 px-6 py-4">
       <div
         class="flex items-center justify-between pb-4 border-b border-dashed border-gray-300"
       >
@@ -8,9 +8,9 @@
           <BuildingOffice2Icon class="h-8 w-8 text-blue-500" />
           <h4 class="text-xl font-semibold">Attendanc</h4>
         </div>
-        <span class="text-xs">v0.1.1</span>
+        <ChevronDownIcon class="h-4 w-4 mt-1 text-gray-500" />
       </div>
-      <ul class="flex flex-col items-start space-y-5 pt-12">
+      <ul class="flex flex-col items-start space-y-5 pt-8">
         <li class="flex items-center space-x-3">
           <HomeIcon class="h-6 w-6 text-gray-400" />
           <span>Dasboard</span>
@@ -27,7 +27,7 @@
           <span>Members</span>
         </li>
         <li
-          class="flex items-center space-x-3 py-2 rounded-md bg-white w-full font-medium"
+          class="flex items-center space-x-3 py-2 rounded-md bg-white px-2 w-full font-medium"
         >
           <ClipboardDocumentCheckIcon class="h-6 w-6 text-gray-700" />
           <span>Attendance</span>
@@ -48,7 +48,7 @@
         <PlusCircleIcon class="h-6 w-6 text-gray-400" />
         <span>Create Qrcode</span>
       </div>
-      <div class="absolute bottom-10 h-16 mb-10">
+      <div class="absolute bottom-10 h-10 mb-10">
         <div class="flex items-center space-x-2 text-sm pb-4">
           <InformationCircleIcon class="h-6 w-6" />
           <span>Help & Getting Started</span>
@@ -73,7 +73,82 @@
         </ul>
       </div>
     </aside>
-    <main class="flex-1 bg-white rounded-md"></main>
+    <main class="flex-1 flex-col bg-white rounded-md shadow">
+      <header
+        class="flex items-center justify-between px-8 py-4 border-b border-dashed border-gray-200"
+      >
+        <h4 class="text-xl font-semibold">Dashboard</h4>
+        <div class="flex items-center space-x-6">
+          <MagnifyingGlassIcon class="h-6 w-6 text-gray-500" />
+          <span class="text-sm">Sunday, 7 November 2023</span>
+          <div class="relative">
+            <BellIcon class="h-6 w-6 text-gray-500" />
+            <div
+              class="absolute top-0 right-1 rounded-full h-2 w-2 bg-red-500"
+            ></div>
+          </div>
+          <div class="flex items-center space-x-3">
+            <img
+              src="https://eu.ui-avatars.com/api/?background=random&name=Bernard+Kissi"
+              class="rounded-full h-8 w-8"
+            />
+            <span class="text-sm">Bernard Kissi</span>
+            <ChevronDownIcon class="h-4 w-4 font-medium text-gray-400" />
+          </div>
+        </div>
+      </header>
+      <section class="flex pt-8 px-8 space-x-8">
+        <div class="flex flex-col w-64">
+          <div
+            class="flex flex-col items-center p-4 w-64 mb-8 bg-gray-100 rounded"
+          >
+            <div class="flex items-center justify-between space-x-8 pb-2">
+              <span class="text-sm font-semibold">24, April 2023</span>
+              <div
+                class="flex items-center text-green-600 text-sm font-semibold space-x-2"
+              >
+                <div class="rounded-full h-2 w-2 bg-green-500"></div>
+                <span>Live</span>
+              </div>
+            </div>
+            <img src="images/qrcode.png" alt="qrcode" class="h-48 w-48" />
+            <div class="pt-2">
+              <p class="text-sm font-medium">Expires after 4 hours</p>
+              <p class="text-sm text-blue-400">http://sigmund.name</p>
+            </div>
+          </div>
+          <div
+            class="flex items-center justify-center space-x-2 border border-dashed border-gray-300 rounded py-3 px-2 hover:border-gray-600"
+          >
+            <PlusCircleIcon class="h-6 w-6 text-gray-400" />
+            <span>Create Qrcode</span>
+          </div>
+        </div>
+        <div class="flex-1 flex-col">
+          <h4 class="text-2xl font-bold">Attendance Summary</h4>
+          <div class="flex item-center justify-between py-6">
+            <div
+              class="bg-gray-50 px-3 py-3 text-sm font-semibold text-gray-500 rounded-md shadow-sm"
+            >
+              Dec 7, 2021 - Dec 2, 2021
+            </div>
+            <div class="flex items-center space-x-5">
+              <CalendarIcon
+                class="h-10 w-10 bg-gray-50 rounded-full px-1 py-2 shadow-md"
+              />
+              <AdjustmentsHorizontalIcon
+                class="h-10 w-10 bg-gray-50 rounded-full px-1 py-2 shadow-md"
+              />
+            </div>
+          </div>
+          <VueApexCharts
+            height="300"
+            :options="chart.chartOptions"
+            :series="chartData.series"
+          />
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 <script setup>
@@ -89,5 +164,79 @@ import {
   PlusCircleIcon,
   InformationCircleIcon,
   CalendarIcon,
+  ChevronDownIcon,
+  BellIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsHorizontalIcon,
 } from "@heroicons/vue/24/outline";
+import { reactive } from "vue";
+import VueApexCharts from "vue3-apexcharts";
+
+const chartData = reactive({
+  series: [
+    {
+      name: "Net Profit",
+      data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+    },
+    {
+      name: "Revenue",
+      data: [76, 85, 101, 98, 87, 90, 91, 98, 94],
+    },
+  ],
+});
+const chart = reactive({
+  chartOptions: {
+    chart: {
+      animations: {
+        enabled: true,
+      },
+      type: "bar",
+      stacked: false,
+      fontFamily: '"Inter", sans-serif',
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "30%",
+        endingShape: "rounded",
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ["transparent"],
+    },
+    xaxis: {
+      categories: [
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+      ],
+    },
+    yaxis: {
+      title: {
+        text: "$ (thousands)",
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return "$ " + val + " thousands";
+        },
+      },
+    },
+  },
+});
 </script>
