072ddb3f7bed713a6865b2d21ec0a76ea7a59143
fkaaziebu <frederickaziebu1998@gmail.com>
2024-03-22 19:50:45 +0000

    feat: create Manage Service Offcanvas component

--- resources/js/Pages/Tenant/TenantServiceDetail.vue
+++ resources/js/Pages/Tenant/TenantServiceDetail.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
 
 import BaseLayout from '@/Layouts/BaseLayout.vue'
 import StatsCard from '@/Components/Cards/StatsCard.vue'
@@ -45,6 +46,7 @@ import {
   EyeIcon,
   PencilSquareIcon,
   Cog8ToothIcon,
+  XMarkIcon,
 } from '@heroicons/vue/24/outline'
 
 // state
@@ -52,6 +54,7 @@ const chartData = ref([])
 const selectedServices = ref([])
 const data = ref(qrcodesAPI)
 const graphData = ref(attendanceApi)
+const service_type = ref('time_bound')
 
 const toggle = () => {
   const { toggleAllCheckBoxes, selectedCheckedBoxes } =
@@ -71,6 +74,7 @@ const onInitBaseChart = () => {
 
 onMounted(() => {
   onInitBaseChart()
+  initFlowbite()
 })
 </script>
 <template>
@@ -211,10 +215,165 @@ onMounted(() => {
             <QrCodeIcon class="h-5 w-5 text-white" />
             <span>Add Qrcode</span>
           </PrimaryButton>
-          <PrimaryButton selector="demo-button" class="bg-gray-800 text-white">
+
+          <!-- Offcanvas Start -->
+          <button
+            id="staticFilter"
+            type="button"
+            class="flex items-center space-x-3 rounded-md bg-gray-800 px-4 py-3 text-white"
+            data-drawer-target="drawer-right-example"
+            data-drawer-show="drawer-right-example"
+            data-drawer-placement="right"
+            aria-controls="drawer-right-example"
+          >
             <Cog8ToothIcon class="h-5 w-5 text-white" />
             <span>Manage</span>
-          </PrimaryButton>
+          </button>
+
+          <div
+            id="drawer-right-example"
+            class="fixed right-0 top-0 z-40 h-screen w-[35%] translate-x-full overflow-y-auto bg-white p-4 transition-transform dark:bg-gray-800"
+            tabindex="-1"
+            aria-labelledby="drawer-right-label"
+          >
+            <div class="border-b border-dashed">
+              <h5
+                id="drawer-right-label"
+                class="mb-4 inline-flex items-center text-lg font-semibold text-gray-700 dark:text-gray-400"
+              >
+                <div class="flex items-center">
+                  <span>Manage Service</span>
+                </div>
+              </h5>
+              <button
+                type="button"
+                data-drawer-hide="drawer-right-example"
+                aria-controls="drawer-right-example"
+                class="absolute end-2.5 top-2.5 inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                <XMarkIcon class="h-4 w-4 text-gray-950" />
+                <span class="sr-only">Close menu</span>
+              </button>
+            </div>
+
+            <div class="mt-4 flex flex-col gap-10">
+              <div class="flex flex-col gap-4">
+                <div class="flex items-end gap-3">
+                  <div class="flex w-2/3 flex-col gap-2">
+                    <label for="name" class="text-sm"> Name </label>
+                    <input
+                      id="name"
+                      type="text"
+                      placeholder="Koinonia service"
+                      class="relative w-full rounded-md border border-gray-200 px-8 py-[0.9rem] text-sm placeholder-gray-400"
+                    />
+                  </div>
+                  <div class="flex w-1/3 flex-col gap-2">
+                    <label for="service_type">Service Type</label>
+                    <select
+                      name="service_type"
+                      id="service_type"
+                      v-model="service_type"
+                      class="rounded-md border border-gray-200 px-8 py-[0.9rem] text-sm text-gray-400"
+                    >
+                      <option value="time_bound">Time bound</option>
+                      <option value="recurring">Recurring</option>
+                      <option value="one_time">One Time</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="flex flex-col items-start gap-2">
+                  <label for="description" class="text-sm"> Description </label>
+                  <textarea
+                    id="description"
+                    type="text"
+                    placeholder="Koinonia service which is held every sunday evening"
+                    class="relative w-full rounded-md border border-gray-200 px-8 py-[0.9rem] text-sm placeholder-gray-400"
+                  />
+                </div>
+              </div>
+
+              <div class="flex flex-col gap-4">
+                <div class="flex items-center gap-3">
+                  <div
+                    v-if="service_type != 'recurring'"
+                    class="flex w-full flex-col gap-2"
+                  >
+                    <label for="start_date" class="text-sm"> Start date </label>
+                    <input
+                      id="start_date"
+                      type="date"
+                      class="relative w-full rounded-md border border-gray-200 px-8 py-[0.9rem] text-sm placeholder-gray-400"
+                    />
+                  </div>
+                  <div v-else class="flex w-full flex-col gap-2">
+                    <label for="day" class="text-sm"> Day </label>
+                    <select
+                      name="day"
+                      id="day"
+                      class="w-[50%] rounded-md border border-gray-200 px-8 py-[0.9rem] text-sm text-gray-400"
+                    >
+                      <option value="sunday">Sunday</option>
+                      <option value="monday">Monday</option>
+                      <option value="tuesday">Tuesday</option>
+                      <option value="wednesday">Wednesday</option>
+                      <option value="thursday">Thursday</option>
+                      <option value="friday">Friday</option>
+                      <option value="saturday">Saturday</option>
+                    </select>
+                  </div>
+
+                  <div
+                    v-if="service_type != 'recurring'"
+                    class="flex w-full flex-col gap-2"
+                  >
+                    <label for="end_date" class="text-sm"> End date </label>
+                    <input
+                      id="end_date"
+                      type="date"
+                      class="relative w-full rounded-md border border-gray-200 px-8 py-[0.9rem] text-sm placeholder-gray-400"
+                    />
+                  </div>
+                  <div v-else></div>
+                </div>
+                <div class="flex items-center gap-3">
+                  <div class="flex w-full flex-col gap-2">
+                    <label for="start_time" class="text-sm"> Start time </label>
+                    <input
+                      id="start_time"
+                      type="time"
+                      class="relative w-full rounded-md border border-gray-200 px-8 py-[0.9rem] text-sm placeholder-gray-400"
+                    />
+                  </div>
+                  <div class="flex w-full flex-col gap-2">
+                    <label for="end_time" class="text-sm"> End time </label>
+                    <input
+                      id="end_time"
+                      type="time"
+                      class="relative w-full rounded-md border border-gray-200 px-8 py-[0.9rem] text-sm placeholder-gray-400"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="absolute bottom-4 end-2.5 flex w-full gap-4 pl-5">
+              <button
+                type="button"
+                data-drawer-hide="drawer-right-example"
+                aria-controls="drawer-right-example"
+                class="flex w-full items-center justify-center rounded-md border border-gray-200 bg-gray-100 px-4 py-3 text-green-950"
+              >
+                Cancel
+              </button>
+              <button
+                class="flex w-full items-center justify-center rounded-md border border-gray-200 bg-green-300 px-4 py-3 text-green-950"
+              >
+                Save Changes
+              </button>
+            </div>
+          </div>
+          <!-- Offcanvas End -->
         </div>
       </div>
       <!-- chart area-->
